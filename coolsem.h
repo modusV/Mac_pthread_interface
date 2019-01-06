ed8f1045386c9d2b82e5c627fac61720e253f4de
Lorenzo Santolini <santolini.lorenzo@gmail.com>
2019-01-06 15:29:24 +0100

    Semaphore interoperable library added

---
+++ coolsem.h
@@ -0,0 +1,25 @@
+//
+// Created by Lorenzo Santolini on 06/01/2019.
+//
+
+#ifndef MYSOLUTION_COOLSEM_H
+#define MYSOLUTION_COOLSEM_H
+
+#include <stdint.h>
+#include <dispatch/dispatch.h>
+#include <semaphore.h>
+
+struct cool_sem {
+#ifdef __APPLE__
+    dispatch_semaphore_t    sem;
+#else
+    sem_t                   sem;
+#endif
+};
+
+
+void rk_sem_init(struct cool_sem *s, uint32_t value);
+void rk_sem_wait(struct cool_sem *s);
+void rk_sem_post(struct cool_sem *s);
+
+#endif //MYSOLUTION_COOLSEM_H
